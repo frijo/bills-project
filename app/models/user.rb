07724e0a2e6be6f2cf0f7b0a5ed8acf436037cc0
frijo <frijo45@gmail.com>
2016-01-24 02:58:33 -0600

    Navbar and signup template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+  validates_confirmation_of :nick_name, :password
+    validates_confirmation_of :email,
+                              message: 'should match confirmation'
 end
