3e69d224ac8604c9f03e0e433477d69220816031
frijo <frijo45@gmail.com>
2016-01-25 03:25:31 -0600

    Adding Login Validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,13 @@ class UsersController < ApplicationController
 	     # session[:user_id]=@user.id
 	      redirect_to '/'
 	    else
-	      redirect_to '/signup'
+	      render 'new'
 	    end
 	end
 	
 	private
   	def user_params
-    	params.required(:user).permit(:nick_name,:first_name,:last_name,:email,:password,:Isadmin,:password_confirmation)
+  		# debugger # ---> Use to debug the app in console
+    	  params.required(:user).permit(:nick_name,:first_name,:last_name,:email,:password,:admin,:password_confirmation)
   	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
 	has_secure_password
+	
+	validates :nick_name, uniqueness:{message: "Este nombre de usuario esta en uso"}
+	
+	validates :email, uniqueness:{message: "Este email ya se encuentra registrado"}  
+  	
+  	#validates_confirmation_of :password,message: 'Las contraseñas deben coincidir'
+  	
+ 	validates :password, confirmation: {message: "Las contraseñas deben coincidir"}
+  	#validates :password_confirmation, presence: true
 
-  validates_confirmation_of :nick_name, :password
-    validates_confirmation_of :email,
-                              message: 'should match confirmation'
+  	validates :first_name, length: { minimum: 4,
+    too_short: "El nombre es muy corto, debe contener minimo %{count} letras" }
+  	
+  	validates :first_name, length: { maximum: 30,
+    too_long: "El nombre es muy largo, el maximo es de  %{count} letras" }
+
+
+  
+
 end
