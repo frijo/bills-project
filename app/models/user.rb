fc76a92c8ecf0b6ddd45185c8729ba2d132e2833
frijo <frijo45@gmail.com>
2016-01-31 02:23:12 -0600

    Validations v 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 # encoding: utf-8
 class User < ActiveRecord::Base
 	has_secure_password
-	
+	validates :nick_name, :email,:first_name,:last_name, :email, presence:{message: "Este campo no puede estar en blanco"}
 	validates :nick_name, uniqueness:{message: "Este nombre de usuario esta en uso"}
 	
 	validates :email, uniqueness:{message: "Este email ya se encuentra registrado"}  
   	
-  	#validates_confirmation_of :password,:message => "Las contraseñas deben coincidir"
+ #	validates_confirmation_of :password,:message => "Las contraseñas deben coincidir"
   	validates :password, confirmation:{ message: " Las contraseñas deben coincidir" }
  	
   	validates :first_name, length: { minimum: 4,
     too_short: "El nombre es muy corto, debe contener minimo %{count} letras", maximum: 30,
     too_long: "El nombre es muy largo, el maximo es de  %{count} letras" }
+
+    validates :last_name, length: { minimum: 4,
+    too_short: "El apellido es muy corto, debe contener minimo %{count} letras", maximum: 30,
+    too_long: "El apellido es muy largo, el maximo es de  %{count} letras" }
   	
   	
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 u1 = User.create(nick_name:"Admin",first_name: "David", last_name: "Arroyo", email: "frijo45@hotmail.com", password: "passAdmin",admin: true,profile_photo:"http://usercontent2.hubimg.com/9137907.png")
 u2 = User.create(nick_name:"wolfhot",first_name: "Davida", last_name: "Arroyoa", email: "frijo45@gmail.com", password: "passAdmin",admin: true,profile_photo:"http://usercontent2.hubimg.com/9137907.png")
-#fc1= FoodCategory.create(category:"meal")
-#fc2= FoodCategory.create(category:"drinks")
-#f1=Food.create(code_food: "cod-tila",food_name:"Tilapia",cost_price: 2500,sale_price: 4500,food_categories_id:1)
-#f2=Food.create(code_food: "cod-Gas",food_name:"Pepsi",cost_price: 500,sale_price: 1000,food_categories_id:2)
+fc1= FoodCategory.create(category:"meal")
+fc2= FoodCategory.create(category:"drinks")
+f1=Food.create(code_food: "cod-tila",food_name:"Tilapia",cost_price: 2500,sale_price: 4500,food_categories_id:1)
+f2=Food.create(code_food: "cod-Gas",food_name:"Pepsi",cost_price: 500,sale_price: 1000,food_categories_id:2)
 
