e53939098eb3c9960afc2b308ba508ab22850d31
frijo <frijo45@gmail.com>
2016-02-09 03:07:06 -0600

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,7 @@ Rails.application.configure do
 
   #Permit show image on the server Heroku
 
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
+config.serve_static_assets=false
   
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
