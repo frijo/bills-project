c783e14e69710c6486fd697f558aecb8cb0d4052
frijo <frijo45@gmail.com>
2016-02-09 01:39:53 -0600

    Fixing bug of profile image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  #Permit show image on the server Heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
