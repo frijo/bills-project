cb43125be9069d4962083215b7964af2a14d4d14
frijo <frijo45@gmail.com>
2016-02-11 13:46:16 -0600

    test4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
 
-  config.cache_classes = true #chaging value to true this allow show the image in heroku
+  config.cache_classes = false #chaging value to true this allow show the image in heroku
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
