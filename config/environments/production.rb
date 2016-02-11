942f9f2030b931a2cee318af0ca5571478d5ba1e
frijo <frijo45@gmail.com>
2016-02-11 13:42:29 -0600

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   
-  config.assets.digest = true  #chaging value to true this allow show the image in heroku
+  config.assets.digest = false  #chaging value to true this allow show the image in heroku
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
