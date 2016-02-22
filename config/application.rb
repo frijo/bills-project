019747543a306f5f96010b6239b5ec6a07a2c946
frijo <frijo45@gmail.com>
2016-02-22 03:36:18 -0600

    Heroku Amazon S3

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module BillsProject
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
 
-   # config.paperclip_defaults = {
-   #         :storage => :s3,
-   #         :s3_host_name => 's3-us-west-2.amazonaws.com'
-   # }
+   
   end
 end
